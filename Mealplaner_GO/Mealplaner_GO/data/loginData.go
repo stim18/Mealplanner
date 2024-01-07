3100e3dc9873ef33c9d82641d85657289d83f2a8
stim18 <101191242+stim18@users.noreply.github.com>
2024-01-07 19:58:23 +0100

    Delete Mealplaner_GO/Mealplaner_GO directory

--- Mealplaner_GO/Mealplaner_GO/data/loginData.go
+++
@@ -1,7 +0,0 @@
-// Matrikelnummern: 5911189 und 8441837
-package data
-
-type User struct {
-	Username string
-	Password string
-}
--- Mealplaner_GO/Mealplaner_GO/go.mod
+++
@@ -1,6 +0,0 @@
-//Matrikelnummern: 5911189 und 8441837
-module meinProjekt
-
-go 1.22
-
-require golang.org/x/crypto v0.16.0
